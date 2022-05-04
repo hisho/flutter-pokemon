7e3f10bcd08d95347fc02b46a59a925b1cd51e02
hisho <hisho.web@gmail.com>
2022-05-04 22:38:38 +0900

    ✨ add pikachu type

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,34 @@ class MyHomePage extends StatelessWidget {
             child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Image.network(
-            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
-            height: 100,
-            width: 100),
+        Container(
+          color: Colors.red,
+          child: Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(32),
+                child: Image.network(
+                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+                    height: 100,
+                    width: 100),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text(
+                  'No.25',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
         const Text(
           'pikachu',
           style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        const Chip(
+          label: Text('electric'),
+          backgroundColor: Colors.yellow,
         )
       ],
     )));
