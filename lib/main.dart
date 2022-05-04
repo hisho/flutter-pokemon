0889f510ee7a6df57794ddbdfe0782f7785a9146
hisho <hisho.web@gmail.com>
2022-05-05 03:48:01 +0900

    ✨ create pokemon detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokemon/pokemon_detail.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,51 +15,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-          color: Colors.red,
-          child: Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(32),
-                child: Image.network(
-                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
-                    height: 100,
-                    width: 100),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('detail'),
+          onPressed: () => {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => const PokemonDetail(),
               ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text(
-                  'No.25',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ],
-          ),
+            ),
+          },
         ),
-        const Text(
-          'pikachu',
-          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-        ),
-        const Chip(
-          label: Text('electric'),
-          backgroundColor: Colors.yellow,
-        )
-      ],
-    )));
+      ),
+    );
   }
 }
---
+++ lib/pokemon_detail.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class PokemonDetail extends StatelessWidget {
+  const PokemonDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          color: Colors.red,
+          child: Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(32),
+                child: Image.network(
+                    'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png',
+                    height: 100,
+                    width: 100),
+              ),
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: const Text(
+                  'No.25',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Text(
+          'pikachu',
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        const Chip(
+          label: Text('electric'),
+          backgroundColor: Colors.yellow,
+        )
+      ],
+    )));
+  }
+}
