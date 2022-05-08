0128e89cda79fe6679d1be7999243693830438a4
hisho <hisho.web@gmail.com>
2022-05-09 00:03:41 +0900

    ✨ update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pokemon/pokemon_detail.dart';
+import 'package:pokemon/screen/pokemon_list.dart';
+import 'package:pokemon/screen/settings.dart';
 
 void main() => runApp(const MyApp());
 
@@ -34,15 +35,7 @@ class _TopPageState extends State<TopPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ListView.separated(
-          separatorBuilder: (context, index) => Divider(
-            color: Colors.blueGrey.withOpacity(0.5),
-            height: 1,
-          ),
-          padding: EdgeInsets.zero,
-          itemCount: 1000,
-          itemBuilder: (context, index) => PokemonListItem(index: index),
-        ),
+        child: currentIndex == 0 ? const PokemonList() : const Settings(),
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
@@ -60,47 +53,6 @@ class _TopPageState extends State<TopPage> {
               label: 'settings')
         ],
       ),
-    );
-  }
-}
-
-class PokemonListItem extends StatelessWidget {
-  const PokemonListItem({Key? key, required this.index}) : super(key: key);
-
-  final int index;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      selectedTileColor: Colors.red,
-      contentPadding:
-          const EdgeInsets.only(top: 4, bottom: 4, left: 20, right: 20),
-      hoverColor: Colors.black12.withOpacity(0.1),
-      focusColor: Colors.black12.withOpacity(0.1),
-      leading: Container(
-        width: 80,
-        decoration: BoxDecoration(
-          color: Colors.yellow.withOpacity(0.5),
-          image: const DecorationImage(
-            fit: BoxFit.fitWidth,
-            image: NetworkImage(
-                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png'),
-          ),
-        ),
-      ),
-      title: Text(
-        "Pikachu ${index.toString().padLeft(3, '0')}",
-        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-      ),
-      subtitle: const Text('⚡ ️electric'),
-      trailing: const Icon(Icons.navigate_next),
-      onTap: () => {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (BuildContext context) => const PokemonDetail(),
-          ),
-        ),
-      },
     );
   }
 }
---
+++ lib/screen/pokemon_list.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon/pokemon_detail.dart';
+
+class PokemonList extends StatelessWidget {
+  const PokemonList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      separatorBuilder: (context, index) => Divider(
+        color: Colors.blueGrey.withOpacity(0.5),
+        height: 1,
+      ),
+      padding: EdgeInsets.zero,
+      itemCount: 1000,
+      itemBuilder: (context, index) => PokemonListItem(index: index),
+    );
+  }
+}
+
+class PokemonListItem extends StatelessWidget {
+  const PokemonListItem({Key? key, required this.index}) : super(key: key);
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      selectedTileColor: Colors.red,
+      contentPadding:
+          const EdgeInsets.only(top: 4, bottom: 4, left: 20, right: 20),
+      hoverColor: Colors.black12.withOpacity(0.1),
+      focusColor: Colors.black12.withOpacity(0.1),
+      leading: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          color: Colors.yellow.withOpacity(0.5),
+          image: const DecorationImage(
+            fit: BoxFit.fitWidth,
+            image: NetworkImage(
+                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png'),
+          ),
+        ),
+      ),
+      title: Text(
+        "Pikachu ${index.toString().padLeft(3, '0')}",
+        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+      ),
+      subtitle: const Text('⚡ ️electric'),
+      trailing: const Icon(Icons.navigate_next),
+      onTap: () => {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) => const PokemonDetail(),
+          ),
+        ),
+      },
+    );
+  }
+}
